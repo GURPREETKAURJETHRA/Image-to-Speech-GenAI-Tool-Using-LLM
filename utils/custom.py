ac9404ed7b8c8efd7a705fbfb71450013375251f
Gurpreet Kaur Jethra <100081334+GURPREETKAURJETHRA@users.noreply.github.com>
2024-01-10 01:25:50 +0530

    Added utils

---
+++ utils/custom.py
@@ -0,0 +1,11 @@
+css_code: str = """
+    <style>
+    section[data-testid="stSidebar"] > div > div:nth-child(2) {
+        padding-top: 0.75rem !important;
+    }
+    
+    section.main > div {
+        padding-top: 64px;
+    }
+    </style>
+"""
